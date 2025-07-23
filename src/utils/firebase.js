aaa70de58b1547cb28d8896de64fcfdeafda85bc
Amandas1081 <dasaman713@gmail.com>
2025-07-23 06:46:54 +0530

    done

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -17,3 +17,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+
+
+ export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmRhMzNmNzFhOTljMjgyZjE1Yzg5MjkyNTAzODA5MyIsIm5iZiI6MTc1MjUyMzIzNC44NzUsInN1YiI6IjY4NzU2MWUyMjZlM2ZmZGU2NzhlODMyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o13exaWY6tcVxad81ffRvC7sRdhinNObLUt2fmsqT54'
+  }
+};
+
+
